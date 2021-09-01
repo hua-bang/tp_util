f15c23f1ed7ea69b9a834e1407033744c9a3d500
HONGXL <136531152@qq.com>
2021-09-01 12:19:44 +0800

    change notice

--- application/dgut/controller/Notice.php
+++ application/dgut/controller/Notice.php
@@ -7,6 +7,6 @@ namespace app\dgut\controller;
 class Notice
 {
     public function getNotice() {
-        return "请重新绑定班级获取新学期课表";
+        return "请重新绑定班级获取新学期课表，智慧莞工订水系统正在升级暂无法使用，请前往线下订水";
     }
 }
